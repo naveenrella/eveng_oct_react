21e45d74ab55412aa8d852a2564e08c0b03b4d4a
Avyaan <avi@Avyaans-MacBook-Pro.local>
2018-10-04 05:33:13 +0530

    a

---
+++ package/sample.js
@@ -0,0 +1,30 @@
+Es6=> Ecma Script 6  => Es9
+
+var
+let 
+const
+
+function add(a,b){
+    return a+b
+}
+
+let add = (a,b) => { return a+b }
+
+Functional 
+Light weight & less functionality
+
+
+Class based
+Heavy weight with more functionality
+
+
+State => Local variable
+Props
+
+Component1
+State = name
+    ||
+    ||Props
+    ||
+Component2
+State= 
