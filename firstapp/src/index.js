a098ea876d748dc8d066f480cc416c9ba343d5ea
Naveen Reddy Rella <naveenrella@Naveens-MacBook-Air.local>
2018-10-08 21:37:45 -0700

    integrated looping

--- firstapp/src/index.js
+++ firstapp/src/index.js
@@ -15,14 +15,32 @@ class App extends Component {
 
     render(){
         const cardsList = this.state.list.cardsList;
-        const listItems = cardsList.map((card,index) => 
-            console.log("index "+ index +"--> "+JSON.stringify(card.image))
-            // <li>{listItems.image.src}</li>
+        // const listItems = cardsList.map((card,index) => 
+        //     console.log("index "+ index +"--> "+JSON.stringify(card.image))
+        //     // <li>{listItems.image.src}</li>
+        // );
+
+        const listItems = cardsList.map((card) =>
+            // <li>{card.image.src}</li> 
+            <div>
+                <figure className="snip1529">
+                <img src={card.image.src} alt= {card.image.alt} />
+                <div className="date"><span className="day">28</span><span className="month">Apr</span></div>
+                <figcaption>
+                    <h3>{card.text.heading}</h3>
+                    <p>{card.text.description}</p>
+                </figcaption>
+                <div className="hover"><i className="ion-android-open"></i></div>
+                <a href="#"></a>
+            </figure> 
+            </div>
+            
+            
         );
                  
         return (            
             <div >
-                <figure className="snip1529">
+                {/* <figure className="snip1529">
                     <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/pr-sample13.jpg" alt="pr-sample13" />
                     <div className="date"><span className="day">28</span><span className="month">Apr</span></div>
                     <figcaption>
@@ -31,8 +49,9 @@ class App extends Component {
                     </figcaption>
                     <div className="hover"><i className="ion-android-open"></i></div>
                     <a href="#"></a>
-                </figure>
-                <figure className="snip1529"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/pr-sample21.jpg" alt="pr-sample21" />
+                </figure> */}
+                {listItems}
+                {/* <figure className="snip1529"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/pr-sample21.jpg" alt="pr-sample21" />
                     <div className="date"><span className="day">17</span><span className="month">May</span></div>
                     <figcaption>
                         <h3>Down with this sort of thing</h3>
@@ -49,7 +68,7 @@ class App extends Component {
                     </figcaption>
                     <div className="hover"><i className="ion-android-open"></i></div>
                     <a href="#"></a>
-                </figure>
+                </figure> */}
             </div>
         )
         
